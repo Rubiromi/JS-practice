3d7a0eb621ff3cf8cce9835864ff02dd62cbcd1c
Rubiromi <hiromi.deyoung@gmail.com>
2015-04-14 09:39:19 -0600

    added another way to make the code work in order.

--- section2/hoisting.js
+++ section2/hoisting.js
@@ -1,18 +1,40 @@
+// function capacityStatus(numGuests, capacity) {
+//   var noSeats = function() {
+//     alert("No seats left!");
+//     return false;
+//   }
+//   var seatsAvailable = function() {
+//     alert("There are " + (capacity - numGuests) + " seats available!");
+//     return true;
+//   }
+//
+//   if(numGuests == capacity) {
+//     noSeats();
+//   } else {
+//     seatsAvailable();
+//   }
+// }
+//
+// capacityStatus(100, 100);
+
 function capacityStatus(numGuests, capacity) {
-  var noSeats = function() {
-    alert("No seats left!");
-    return false;
-  }
-  var seatsAvailable = function() {
-    alert("There are " + (capacity - numGuests) + " seats available!");
-    return true;
-  }
 
   if(numGuests == capacity) {
     noSeats();
   } else {
     seatsAvailable();
   }
+
+  function noSeats() {
+    alert("No seats left!");
+    return false;
+  }
+  function seatsAvailable() {
+    alert("There are " + (capacity - numGuests) + " seats available!");
+    return true;
+  }
+
+
 }
 
-capacityStatus(77, 100);
+capacityStatus(32, 100);
